856921557ff9760227126549d9c53388c154e0cf
Shashank Shekhar <shashankreeti@yahoo.com>
2021-10-17 14:52:01 +0530

    Basic Infrastructure of Edit Create and About Component

---
+++ client/src/components/CreateComponent/Component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Component.css';
+import {GrAddCircle} from 'react-icons/gr';
+const Component = () =>{
+    return (
+      <div className="write">
+       <img src="https://www.wallpapertip.com/wmimgs/23-236943_us-wallpaper-for-website.jpg" alt="" className="writeImg"/>
+        <form className="writeForm">
+            <div className="writeFormGroup">
+                 <label htmlFor="fileInput">
+                 <GrAddCircle className="writeIcon"/>
+                 </label>
+                <input type="file" id="fileInput"style={{display: 'none'}}/>
+                <input type="text" placeholder="title" className="writeInput" autoFocus={true}/>
+            </div>
+            <div className="writeFormGroup">
+                <textarea placeholder="Tell your Story" type="text" className="writeInput writeText"></textarea>
+            </div>
+            <button className="writeSubmit">Publish</button>
+        </form>
+      </div>
+    )
+}
+
+export default Component;
