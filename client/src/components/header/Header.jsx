0efc19763ac4a6fadabc25afb7538a1b2c332a6c
Ritik Raj <ritik2903raj@gmail.com>
2021-11-06 18:41:25 +0530

    header responsive

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import "./header.css";
 //import { NavLink } from "react-router-dom";
 // import { GiHamburgerMenu } from "react-icons/gi";
@@ -32,4 +32,47 @@ export const Header = () => {
       </nav>
     </>
   );
+}; */
+
+import React, {useState} from 'react';
+import "./header.css"
+import { Link } from "react-router-dom";
+
+export const Header = () => {
+
+    const [isMobile, setIsMobile] = useState(false);
+
+    return (
+        <nav className="navbar">
+          <h3 className="logo">Techblog</h3>
+          <ul className={isMobile ? "nav-links-mobile" : "nav-links"}
+          onClick={()=> setIsMobile(false)}
+          >
+            <Link to="/" className="home">
+              <li>Home</li>
+            </Link>
+            <Link to="/about" className="about">
+              <li>About</li>
+            </Link>
+            <Link to="/skills" className="skills">
+              <li>Skills</li>
+            </Link>
+            <Link to="/contact" className="contact">
+              <li>Contact</li>
+            </Link>
+            <Link to="/signup" className="signup">
+              <li>Sign Up</li>
+            </Link>
+          </ul>
+         <button className="mobile-menu-icon"
+         onClick={() => setIsMobile(!isMobile)}
+         >
+           {isMobile ? (
+             <i className="fas fa-times"></i>
+           ) :(
+             <i className="fas fa-bars"></i>
+           )}
+         </button>
+        </nav>
+    );
 };
